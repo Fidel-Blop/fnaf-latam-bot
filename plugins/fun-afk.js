5f45c23d38e471c240da27c85189ad5f46d9d7ec
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-30 07:14:06 -0300

    Update fun-afk.js

--- plugins/fun-afk.js
+++ plugins/fun-afk.js
@@ -1,11 +1,19 @@
-const handler = async (m, {text}) => {
-const user = global.db.data.users[m.sender];
-user.afk = + new Date;
-user.afkReason = text;
-conn.reply(m.chat, `${emoji} *El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
-`, m);
+const handler = async (m, { text, conn }) => {
+  const user = global.db.data.users[m.sender];
+  user.afk = +new Date;
+  user.afkReason = text;
+
+  const nombreUsuario = await conn.getName(m.sender);
+  const emoji = '📡';
+
+  conn.reply(
+    m.chat,
+    `${emoji} *Registro de ausencia activado*\n\n🎥 Operador: *${nombreUsuario}*\n🔒 Estado: *Modo inactivo activado*\n🧠 Motivo asignado: *${text ? text : 'No especificado'}*\n\n⚙️ El monitoreo automático se ajustará hasta nuevo aviso.\n\n— Sistema respaldado por FNaF LATAM™`,
+    m
+  );
 };
-handler.help = ['afk [alasan]'];
+
+handler.help = ['afk [motivo]'];
 handler.tags = ['main'];
 handler.command = ['afk'];
 handler.group = true;
