d36c83abbcdc1294ad6c0f30476993c8f62037fe
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-30 07:15:57 -0300

    Update fun-amigo-random.js

--- plugins/fun-amigo-random.js
+++ plugins/fun-amigo-random.js
@@ -1,17 +1,27 @@
-let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`${emoji} Vamos a hacer algunas amistades.\n\n*Oye ${toM(a)} hablale al privado a ${toM(b)} para que jueguen y se haga una amistad 🙆*\n\n*Las mejores amistades empiezan con un juego 😉.*`, null, {
-mentions: [a, b]
-})}
-handler.help = ['amistad']
-handler.tags = ['fun']
-handler.command = ['amigorandom','amistad']
-handler.group = true
-handler.register = true
+let toM = a => '@' + a.split('@')[0];
 
-export default handler
+function handler(m, { groupMetadata }) {
+  let ps = groupMetadata.participants.map(v => v.id);
+  let a = ps.getRandom();
+  let b;
+  do b = ps.getRandom();
+  while (b === a);
+
+  const emoji = '👁';
+
+  m.reply(
+    `${emoji} *Sistema de vinculación social activado*\n\n📡 Asignación en curso...\n🦴 *Unidad ${toM(a)}* ha sido seleccionada para iniciar contacto con *Unidad ${toM(b)}*.\n\n🔊 *Instrucción*: Establecer comunicación directa en el canal privado y comenzar proceso de interacción amistosa.\n\n🎮 *Nota*: "Las mejores conexiones surgen jugando... incluso si alguien observa desde las cámaras."\n\n— Protocolo generado por el subsistema de relaciones de FNaF LATAM™`,
+    null,
+    {
+      mentions: [a, b]
+    }
+  );
+}
+
+handler.help = ['amistad'];
+handler.tags = ['fun'];
+handler.command = ['amigorandom', 'amistad'];
+handler.group = true;
+handler.register = true;
+
+export default handler;
