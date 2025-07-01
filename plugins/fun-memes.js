86034c59005b76f82c60ebc0b861880495a0e53a
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-01 00:04:48 -0300

    Update fun-memes.js

--- plugins/fun-memes.js
+++ plugins/fun-memes.js
@@ -1,14 +1,21 @@
 import hispamemes from 'hispamemes'
-let handler = async (m, { conn, usedPrefix, command }) => {
-const meme = hispamemes.meme()
-conn.sendFile(m.chat, meme, '', '', m)
-m.react(emoji2)
+
+let handler = async (m, { conn }) => {
+  const meme = hispamemes.meme()
+
+  await conn.sendMessage(m.chat, {
+    image: { url: meme },
+    caption: `🎥 *Sistema de entretenimiento activado...* ⚙️\n\n📡 Recuperando archivo de humor visual desde la base de datos de *FazbearNet*...\n\n✅ *Transferencia completada.*\n\n🧠 *Unidad: MEME-CORE*\n🔐 Clasificación: Humor inestable\n\n— Sistema de Proyección FNaF LATAM™`,
+  }, { quoted: m })
+
+  m.react('📡') // Emoji de escaneo o sincronización
 }
+
 handler.help = ['meme']
 handler.tags = ['fun']
 handler.command = ['meme', 'memes']
 handler.coin = 1
-handler.group = true;
+handler.group = true
 handler.register = true
 
 export default handler
