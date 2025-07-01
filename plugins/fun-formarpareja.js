a1c85761b1908a262a17e62bd51b235d0efba334
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-06-30 23:57:04 -0300

    Update fun-formarpareja.js

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -1,14 +1,30 @@
 const toM = (a) => '@' + a.split('@')[0];
-function handler(m, {groupMetadata}) {
+
+function handler(m, { groupMetadata }) {
   const ps = groupMetadata.participants.map((v) => v.id);
   const a = ps.getRandom();
   let b;
   do b = ps.getRandom();
   while (b === a);
-  m.reply(`*${toM(a)}, Deberías casarte 💍 con ${toM(b)}, hacen una bonita pareja 💓*`, null, {
+
+  const mensaje = `🎥 *Monitoreo de compatibilidad activado...*\n\n📡 Emparejamiento detectado por la Unidad Sentimental Fazbear™\n\n💍 ${toM(a)}, deberías vincularte emocionalmente con ${toM(b)}.\n\n📈 *Nivel de sincronización afectiva:* ${pickRandom([
+    '82%',
+    '94%',
+    '69%',
+    '100%',
+    '47%',
+    '76%',
+    '91%',
+    '88%',
+    '50%',
+    '99%'
+  ])}\n\n💓 *Observación:* “Combinación validada por el sistema de emociones supervisadas de FNaF LATAM™.”\n\n⛓ *Monitoreo en curso...*`;
+
+  m.reply(mensaje, null, {
     mentions: [a, b],
   });
 }
+
 handler.help = ['formarpareja'];
 handler.tags = ['fun'];
 handler.command = ['formarpareja', 'formarparejas'];
@@ -16,3 +32,7 @@ handler.group = true;
 handler.register = true;
 
 export default handler;
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
