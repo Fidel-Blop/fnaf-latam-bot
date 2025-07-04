02c888e60aa55941914ed30fdb446f75f9114cb2
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-04 01:25:09 -0300

    Update tools-cal.js

--- plugins/tools-cal.js
+++ plugins/tools-cal.js
@@ -1,39 +1,44 @@
 let handler = async (m, { conn, text }) => {
   let id = m.chat
-  conn.math = conn.math ? conn.math : {}
+  conn.math = conn.math || {}
+
   if (id in conn.math) {
     clearTimeout(conn.math[id][3])
     delete conn.math[id]
-    m.reply('.... ')
+    return m.reply('⏹️ Se canceló la operación anterior.')
   }
+
+  if (!text) return m.reply(`✏️ Ingresa una ecuación para calcular.\n\nEjemplo: *3 × (2 + π) ÷ 4*`)
+
   let val = text
-    .replace(/[^0-9\-\/+*×÷πEe()piPI/]/g, '')
-    .replace(/×/g, '*')
-    .replace(/÷/g, '/')
+    .replace(/[^0-9πpieE()+\-*/×÷.^]/gi, '')
     .replace(/π|pi/gi, 'Math.PI')
     .replace(/e/gi, 'Math.E')
-    .replace(/\/+/g, '/')
-    .replace(/\++/g, '+')
-    .replace(/-+/g, '-')
-  let format = val
+    .replace(/×/g, '*')
+    .replace(/÷/g, '/')
+    .replace(/\^/g, '**') // potencia
+    .replace(/--/g, '+') // doble negativo simplificado
+
+  let format = text
+    .replace(/\*/g, '×')
+    .replace(/\//g, '÷')
     .replace(/Math\.PI/g, 'π')
     .replace(/Math\.E/g, 'e')
-    .replace(/\//g, '÷')
-    .replace(/\*×/g, '×')
+
   try {
-    console.log(val)
-    let result = (new Function('return ' + val))()
-    if (!result) throw result
-    m.reply(`*${format}* = _${result}_`)
+    const result = (new Function('return ' + val))()
+    if (result === undefined || isNaN(result)) throw new Error("Resultado no válido")
+
+    return m.reply(`🧮 *${format}* = _${result}_`)
   } catch (e) {
-    if (e == undefined) return m.reply(`${emoji} Ingresa la ecuación.\nsímbolos compatibles -, +, *, /, ×, ÷, π, e, (, )`)
-    return m.reply(`${emoji2} Formato incorrecto, solo 0-9 y símbolo -, +, *, /, ×, ÷, π, e, (, ) que puedes usar`)
+    return m.reply(`⚠️ Ecuación inválida.\n\nSolo se aceptan:\n- Números\n- Paréntesis ( )\n- Operadores: + - × ÷ * /\n- Constantes: π, e`)
   }
 }
-handler.help = ['cal *<ecuacion>*']
+
+handler.help = ['cal <ecuación>']
 handler.tags = ['tools']
-handler.command = ['cal', 'calc', 'calcular', 'calculadora'] 
+handler.command = ['cal', 'calc', 'calcular', 'calculadora']
 handler.exp = 5
-handler.register = true 
+handler.register = true
 
 export default handler
