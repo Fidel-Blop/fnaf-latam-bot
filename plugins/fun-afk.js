9ed1624ec54f8eee2fb0ac062e49c8978ef858f7
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-12 20:00:39 -0300

    Update fun-afk.js

--- plugins/fun-afk.js
+++ plugins/fun-afk.js
@@ -1,11 +1,10 @@
-const handler = async (m, {text}) => {
-const user = global.db.data.users[m.sender];
-user.afk = + new Date;
-user.afkReason = text;
-conn.reply(m.chat, `${emoji} *El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
-`, m);
+const handler = async (m, { text, conn }) => {
+  const user = global.db.data.users[m.sender];
+  user.afk = +new Date();
+  user.afkReason = text;
+  conn.reply(m.chat, `⚠️ *El animatrónico ${conn.getName(m.sender)} ha entrado en modo inactivo.*\n\n*Motivo:* ${text ? text : 'Sin especificar.'}`, m);
 };
-handler.help = ['afk [alasan]'];
+handler.help = ['afk [motivo]'];
 handler.tags = ['main'];
 handler.command = ['afk'];
 handler.group = true;
