f245313b514f46d11bfc4ed98020d5dc1815e052
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-12 20:32:42 -0300

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -1,19 +1,22 @@
 var handler = async (m, { conn, command, text }) => {
 
-if (!text) return conn.reply(m.chat, `${emoji} Escribe tu nombre y el nombre de la otra personas para calcular su amor.`, m)
-let [text1, ...text2] = text.split(' ')
+  if (!text) return conn.reply(m.chat, '💔 *[FNaF LATAM]*\n\n📎 Por favor, escribí tu nombre y el de otra persona para analizar la compatibilidad.', m)
 
-text2 = (text2 || []).join(' ')
-if (!text2) return conn.reply(m.chat, `${emoji2} Escribe el nombre de la segunda persona.`, m)
-let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de ${Math.floor(Math.random() * 100)}% 👩🏻‍❤️‍👨🏻`
+  let [text1, ...text2] = text.split(' ')
+  text2 = (text2 || []).join(' ')
 
-m.reply(love, null, { mentions: conn.parseMention(love) })
+  if (!text2) return conn.reply(m.chat, '📌 *[FNaF LATAM]*\n\n💬 Ingresá también el nombre de la segunda persona para completar el escaneo.', m)
 
+  let porcentaje = Math.floor(Math.random() * 100)
+  let love = `🫀 *[FNaF LATAM - Scanner de Compatibilidad]*\n\n🔍 Analizando nombres...\n\n🧸 *${text1}* y *${text2}* tienen una compatibilidad del *${porcentaje}%*.\n\n📊 Resultado calculado por el sistema de IA animatrónica.`
+
+  m.reply(love, null, { mentions: conn.parseMention(love) })
 }
+
 handler.help = ['ship', 'love']
 handler.tags = ['fun']
 handler.command = ['ship','pareja']
-handler.group = true;
+handler.group = true
 handler.register = true
 
 export default handler
