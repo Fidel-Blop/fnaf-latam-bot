acc3e5083d36376a4b669e46949fa33e5ca9ff67
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-12 20:34:52 -0300

    Update fun-top.js

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -1,44 +1,40 @@
 import util from 'util'
 import path from 'path'
+
 let user = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese un texto para hacer un Top 10 *texto*.`, m)
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b = ps.getRandom()
-let c = ps.getRandom()
-let d = ps.getRandom()
-let e = ps.getRandom()
-let f = ps.getRandom()
-let g = ps.getRandom()
-let h = ps.getRandom()
-let i = ps.getRandom()
-let j = ps.getRandom()
-let k = Math.floor(Math.random() * 70);
-let x = `${pickRandom(['🤓','😅','😂','😳','😎', '🥵', '😱', '🤑', '🙄', '💩','🍑','🤨','🥴','🔥','👇🏻','😔', '👀','🌚'])}`
-let l = Math.floor(Math.random() * x.length);
-let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
-let top = `*${x} Top 10 ${text} ${x}*
-    
-*1. ${user(a)}*
-*2. ${user(b)}*
-*3. ${user(c)}*
-*4. ${user(d)}*
-*5. ${user(e)}*
-*6. ${user(f)}*
-*7. ${user(g)}*
-*8. ${user(h)}*
-*9. ${user(i)}*
-*10. ${user(j)}*`
-m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
+
+function handler(m, { groupMetadata, command, conn, text, usedPrefix }) {
+  if (!text) return conn.reply(m.chat, '🎮 *[FNaF LATAM]*\n\n📌 Ingresá un tema para hacer el Top 10. Ejemplo: *.top animatrónicos más sospechosos*', m)
+
+  let participantes = groupMetadata.participants.map(v => v.id)
+  let a = participantes.getRandom()
+  let b = participantes.getRandom()
+  let c = participantes.getRandom()
+  let d = participantes.getRandom()
+  let e = participantes.getRandom()
+  let f = participantes.getRandom()
+  let g = participantes.getRandom()
+  let h = participantes.getRandom()
+  let i = participantes.getRandom()
+  let j = participantes.getRandom()
+
+  let k = Math.floor(Math.random() * 70)
+  let emoji = pickRandom(['🦊', '🐻', '🎤', '🔧', '⚙️', '💀', '🔦', '👀', '📸', '📟', '🩸', '📍'])
+  let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
+
+  let resultado = `🎟️ *[TOP 10 – ${text.toUpperCase()}]* 🎟️\n_Animatrónicos en acción..._\n\n${emoji} *1.* ${user(a)}\n${emoji} *2.* ${user(b)}\n${emoji} *3.* ${user(c)}\n${emoji} *4.* ${user(d)}\n${emoji} *5.* ${user(e)}\n${emoji} *6.* ${user(f)}\n${emoji} *7.* ${user(g)}\n${emoji} *8.* ${user(h)}\n${emoji} *9.* ${user(i)}\n${emoji} *10.* ${user(j)}\n\n⚙️ *Sistema FNaF LATAM – Clasificación completa*`
+
+  m.reply(resultado, null, { mentions: [a, b, c, d, e, f, g, h, i, j] })
 }
-handler.help = ['top *<texto>*']
+
+handler.help = ['top *<tema>*']
 handler.command = ['top']
 handler.tags = ['fun']
-handler.group = true;
+handler.group = true
 handler.register = true
 
 export default handler
 
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+  return list[Math.floor(Math.random() * list.length)]
+}
