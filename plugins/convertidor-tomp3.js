17553d26ac78cea7ab2b2e89de5fb2a768126a0e
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-12 17:35:51 -0300

    Update convertidor-tomp3.js

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -1,23 +1,30 @@
-import {toAudio, toPTT} from '../lib/converter.js'
+import { toAudio, toPTT } from '../lib/converter.js'
 
-const handler = async (m, {conn, usedPrefix, command}) => {
-const q = m.quoted ? m.quoted : m
-const mime = (q || q.msg).mimetype || q.mediaType || ''
-  
-if (!/video|audio/.test(mime)) {
-return conn.reply(m.chat, `${emoji} Por favor, responda al video o nota de voz que desee convertir a Audio/MP3.`, m)
-}
-  
-const media = await q.download()
-if (!media) {
-return conn.reply(m.chat, `${msm} Ocurrio un error al descargar su video.`, m)
-}
-  
-const audio = await toPTT(media, 'mp4')
-if (!audio.data) {
-return conn.reply(m.chat, `${msm} Ocurrio un error al convertir su nota de voz a Audio/MP3.`, m)
-}
-conn.sendMessage(m.chat, {audio: media || audio.data, mimetype: 'audio/mpeg'}, {quoted: m})
+const handler = async (m, { conn, usedPrefix, command }) => {
+  const firma = '🎭 Respaldado por FNAF LATAM 🎭'
+  const q = m.quoted ? m.quoted : m
+  const mime = (q || q.msg).mimetype || q.mediaType || ''
+
+  if (!/video|audio/.test(mime)) {
+    return conn.reply(m.chat, `🎙️ *Convierte audios o videos a formato MP3*\n\n📌 Responde a un *video* o una *nota de voz* para convertirlo a audio.\n\n${firma}`, m)
+  }
+
+  const media = await q.download()
+  if (!media) {
+    return conn.reply(m.chat, `❌ *Error:* No se pudo descargar el archivo multimedia.\n\n${firma}`, m)
+  }
+
+  const audio = await toPTT(media, 'mp4')
+  if (!audio.data) {
+    return conn.reply(m.chat, `❌ *Error:* Falló la conversión del archivo.\n\n${firma}`, m)
+  }
+
+  conn.sendMessage(m.chat, {
+    audio: media || audio.data,
+    mimetype: 'audio/mpeg'
+  }, {
+    quoted: m
+  })
 }
 
 handler.help = ['tomp3', 'toaudio']
