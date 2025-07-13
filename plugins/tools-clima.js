dba78220de97caf944f6105f5e097d780acd9b33
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 19:58:52 -0300

    Update tools-clima.js

--- plugins/tools-clima.js
+++ plugins/tools-clima.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let handler = async (m, { conn, args }) => {
   if (!args[0]) {
-    let resp = `${emoji} Ingrese el nombre de su País o Ciudad.`;
+    let resp = `🔧 *Sistema de Monitoreo Climático*\n\n📍 Ingrese el nombre de su *ciudad o país* para consultar el clima actual.\n\n📝 Ejemplo: *clima Buenos Aires*`;
     let txt = '';
     let count = 0;
     for (const c of resp) {
@@ -13,13 +13,14 @@ let handler = async (m, { conn, args }) => {
         conn.sendPresenceUpdate('composing', m.chat);
       }
     }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24*60*60, disappearingMessagesInChat: 24*60*60 });
+    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24 * 60 * 60, disappearingMessagesInChat: 24 * 60 * 60 });
     return;
   }
 
   try {
     const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`);
     const res = response.data;
+
     const name = res.name;
     const Country = res.sys.country;
     const Weather = res.weather[0].description;
@@ -28,7 +29,21 @@ let handler = async (m, { conn, args }) => {
     const Maximum_Temperature = res.main.temp_max + "°C";
     const Humidity = res.main.humidity + "%";
     const Wind = res.wind.speed + "km/h";
-    const wea = `「 📍 」LUGAR: ${name}\n「 🗺️ 」PAIS: ${Country}\n「 🌤️ 」TIEMPO: ${Weather}\n「 🌡️ 」TEMPERATURA: ${Temperature}\n「 💠 」TEMPERATURA MINIMA: ${Minimum_Temperature}\n「 📛 」TEMPERATURA MAXIMA: ${Maximum_Temperature}\n「 💦 」HUMEDAD: ${Humidity}\n「 🌬️ 」VIENTO: ${Wind}`.trim();
+
+    const wea = `
+🎮 *MONITOREO AMBIENTAL - FNaF LATAM*
+━━━━━━━━━━━━━━━━━━━
+📍 *Ubicación:* ${name}
+🗺️ *País:* ${Country}
+🌥️ *Estado del Clima:* ${Weather}
+🌡️ *Temperatura Actual:* ${Temperature}
+🔻 *Mínima:* ${Minimum_Temperature}
+🔺 *Máxima:* ${Maximum_Temperature}
+💧 *Humedad:* ${Humidity}
+🌬️ *Viento:* ${Wind}
+━━━━━━━━━━━━━━━━━━━
+🛑 *Recuerda revisar las ventilaciones, el clima afecta la energía...*
+`.trim();
 
     let txt = '';
     let count = 0;
@@ -40,9 +55,9 @@ let handler = async (m, { conn, args }) => {
         conn.sendPresenceUpdate('composing', m.chat);
       }
     }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24*60*60, disappearingMessagesInChat: 24*60*60 });
+    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24 * 60 * 60, disappearingMessagesInChat: 24 * 60 * 60 });
   } catch (e) {
-    let resp = `${msm} Error!\n No se encontraron resultados, trate de escribir un país o ciudad existente.`;
+    let resp = `⛔ *Error en el monitoreo climático*\n\nNo se encontraron resultados.\n🔎 Asegúrate de ingresar una ciudad o país válido.`;
     let txt = '';
     let count = 0;
     for (const c of resp) {
@@ -53,11 +68,12 @@ let handler = async (m, { conn, args }) => {
         conn.sendPresenceUpdate('composing', m.chat);
       }
     }
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24*60*60, disappearingMessagesInChat: 24*60*60 });
+    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, { quoted: m, ephemeralExpiration: 24 * 60 * 60, disappearingMessagesInChat: 24 * 60 * 60 });
   }
 };
 
-handler.help = ['clima *<ciudad/país>*'];
-handler.tags = ['herramientas'];
+handler.help = ['clima *<ciudad/país>*']
+handler.tags = ['herramientas']
 handler.command = ['clima', 'tiempo']
+
 export default handler;
