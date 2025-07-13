b6b7cf0fe0084f51f86a8b7dd316008e8cf40635
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 01:34:08 -0300

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -2,17 +2,23 @@ import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
-let timestamp = speed()
-let sentMsg = await conn.reply(m.chat, '❀ Calculando ping...', m)
-let latency = speed() - timestamp
-exec(`neofetch --stdout`, (error, stdout, stderr) => {
-let child = stdout.toString("utf-8");
-let ssd = child.replace(/Memory:/, "Ram:")
+  let timestamp = speed()
+  let sentMsg = await conn.reply(m.chat, '🎭 Consultando datos internos del sistema...', m)
+  let latency = speed() - timestamp
+  exec(`neofetch --stdout`, (error, stdout, stderr) => {
+    let child = stdout.toString("utf-8");
+    let ssd = child.replace(/Memory:/, "🔋 Ram:");
 
-let result = `✰ *¡Pong!*\n> Tiempo ⴵ ${latency.toFixed(4).split(".")[0]}ms\n${ssd}`
-conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
-})
+    let result = `╭─⬣『 📡 SISTEMA DE LATENCIA 』⬣─╮
+│ 🩸 *Velocidad:* ${latency.toFixed(4).split(".")[0]} ms
+│
+${ssd}
+╰─⭒───────────────⭒`;
+
+    conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m });
+  });
 }
+
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = ['ping', 'p']
