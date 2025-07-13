b379bf9f4fa96af590b581c7aa090c978e6c1aa4
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 02:43:47 -0300

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -1,23 +1,33 @@
-let handler = async (m, { usedPrefix, command }) => {
-let uptime = await process.uptime()
-let runtime = `${global.botname}
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let uptime = await process.uptime()
+  let runtime = `${global.botname}
 
-✰ Tiempo activo: ${rTime(uptime)}`
-conn.reply(m.chat, runtime, m)
+✰ Estado del sistema: Operando sin interrupciones.
+⏳ Tiempo activo continuo desde el último reinicio:
+${rTime(uptime)}
+
+Gracias por usar FNaF LATAM, el bot que te acompaña en todas tus aventuras.`
+  
+  await conn.sendMessage(m.chat, {
+    text: runtime,
+    contextInfo: {
+      externalAdReply: {
+        title: global.botname,
+        body: 'FNaF LATAM - Manteniéndote siempre informado',
+        thumbnailUrl: 'https://images.adagio.com/images2/custom_blends/230042.jpg', // Cambia acá la URL de la imagen
+        mediaType: 1,
+        showAdAttribution: true,
+        renderLargerThumbnail: true,
+      }
+    }
+  }, { quoted: m })
 }
+
 handler.help = ['runtime']
 handler.tags = ['main']
 handler.command = ['runtime', 'uptime']
 
 export default handler
-
-const dd = new Date(new Date + 3600000);
-const time = dd.toLocaleString('en-US', { 
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-      hour12: true 
-    });
 
 function rTime(seconds) {
   seconds = Number(seconds);
@@ -25,9 +35,9 @@ function rTime(seconds) {
   var h = Math.floor((seconds % (3600 * 24)) / 3600);
   var m = Math.floor((seconds % 3600) / 60);
   var s = Math.floor(seconds % 60);
-  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " Dias, ") : "";
-  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
-  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
+  var dDisplay = d > 0 ? d + (d == 1 ? " día, " : " días, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " horas, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " minutos, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " segundos") : "";
   return dDisplay + hDisplay + mDisplay + sDisplay;
-};
+}
