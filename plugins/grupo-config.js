1cbe428fe2c813c7ae7f5fc325acbe5b556dff4e
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 00:51:52 -0300

    Update grupo-config.js

--- plugins/grupo-config.js
+++ plugins/grupo-config.js
@@ -1,28 +1,31 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono) 
-let isClose = { // Switch Case Like :v
-'open': 'not_announcement',
-'close': 'announcement',
-'abierto': 'not_announcement',
-'cerrado': 'announcement',
-'abrir': 'not_announcement',
-'cerrar': 'announcement',
-}[(args[0] || '')]
-if (isClose === undefined)
-return conn.reply(m.chat, `${emoji} *Elija una opción para configurar el grupo*\n\nEjemplo:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} close*\n*✰ #${command} open*`, m)
-await conn.groupSettingUpdate(m.chat, isClose)
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono);
+  let isClose = { // Comportamiento tipo Switch
+    'open': 'not_announcement',
+    'close': 'announcement',
+    'abierto': 'not_announcement',
+    'cerrado': 'announcement',
+    'abrir': 'not_announcement',
+    'cerrar': 'announcement',
+  }[(args[0] || '')];
 
-if (isClose === 'not_announcement'){
-m.reply(`${emoji} *Ya pueden escribir en este grupo.*`)
-}
+  if (isClose === undefined)
+    return conn.reply(m.chat, `👁️‍🗨️ *Configura el grupo a tu voluntad... pero elige bien.*\n\n📌 Ejemplos:\n*✰ #${command} abrir*\n*✰ #${command} cerrar*\n*✰ #${command} open*\n*✰ #${command} close*`, m);
 
-if (isClose === 'announcement'){
-m.reply(`${emoji2} *Solos los admins pueden escribir en este grupo.*`)
-}}
-handler.help = ['group open / close', 'grupo abrir / cerrar']
-handler.tags = ['grupo']
-handler.command = ['group', 'grupo']
-handler.admin = true
-handler.botAdmin = true
+  await conn.groupSettingUpdate(m.chat, isClose);
 
-export default handler
+  if (isClose === 'not_announcement') {
+    m.reply(`👻 *Las voces vuelven a escucharse... Todos pueden escribir en este grupo.*`);
+  }
+
+  if (isClose === 'announcement') {
+    m.reply(`🛑 *Solo los guardianes de la noche (admins) pueden hablar aquí.*`);
+  }
+};
+handler.help = ['group open / close', 'grupo abrir / cerrar'];
+handler.tags = ['grupo'];
+handler.command = ['group', 'grupo'];
+handler.admin = true;
+handler.botAdmin = true;
+
+export default handler;
