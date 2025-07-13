ed366b7b9c1d7fe75361be0fcd69bee73a84f4d2
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 01:06:27 -0300

    Update grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -1,10 +1,23 @@
 var handler = async (m, { conn, args }) => {
+  let group = m.chat
+  let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '\t\t✿:･✧ link del grupo ✧･:✿\n\n\v' + link, m, { detectLink: true })
-
+  conn.reply(
+    m.chat,
+    `┏━━━━━━◥◣✪◢◤━━━━━━┓
+┃        🎟 *FNaF LATAM* 🎟       
+┃
+┃   🔗 *ENLACE DEL GRUPO* 🔗
+┃
+┃   ${link}
+┃
+┃   ⚠️ No compartas con desconocidos el enlace, podrías pagarlo muy caro. . . Freddy te observa. 👀
+┗━━━━━━◢◤✪◥◣━━━━━━┛`,
+    m,
+    { detectLink: true }
+  )
 }
+
 handler.help = ['link']
 handler.tags = ['grupo']
 handler.command = ['link', 'enlace']
