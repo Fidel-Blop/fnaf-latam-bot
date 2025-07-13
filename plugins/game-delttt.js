7eef14bf314649e4e9dad5848d9ffcf3252edb7b
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 00:28:38 -0300

    Update game-delttt.js

--- plugins/game-delttt.js
+++ plugins/game-delttt.js
@@ -1,12 +1,16 @@
 let handler = async (m, { conn, text }) => {
-	let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`${emoji2} No estás en el juego de TicTacToe .`, m)
-delete conn.game[room.id]
-await conn.reply(m.chat, `${done} Se reinicia la sesión de *tictactoe*.`, m)
+  let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
+  
+  if (!room) return conn.reply(m.chat, `💤 *No estás participando en una partida activa de TicTacToe.*\nUsa *.tictactoe* para iniciar una nueva.`, m)
+
+  delete conn.game[room.id]
+
+  await conn.reply(m.chat, `🔁 *Se ha reiniciado la sesión de TicTacToe.*\nPuedes comenzar una nueva partida cuando quieras con *.tictactoe*`, m)
 }
+
 handler.help = ['delttt']
 handler.tags = ['game']
-handler.command = ['delttc', 'delttt', 'delxo','tictactoe']
+handler.command = ['delttc', 'delttt', 'delxo', 'tictactoe']
 handler.group = true
 handler.register = true
 
