1cc7bd9f98be2877ebcdf0a7238859e2df2bc29c
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 20:02:37 -0300

    Update tools-letra.js

--- plugins/tools-letra.js
+++ plugins/tools-letra.js
@@ -1,40 +1,45 @@
-function handler(m, { text }) {
-if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingresa el texto que quieres transformar.`, m)
+function handler(m, { text, conn, emoji }) {
+  if (!text) return conn.reply(m.chat, `${emoji} ¡Hey! Para transformar el texto, primero debes enviarlo. Intenta de nuevo.`, m);
 
-let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-m.reply(teks.replace(/[a-z]/gi, v => {
-return {
-'a': 'ᥲ',
-'b': 'ᑲ',
-'c': 'ᥴ',
-'d': 'ძ',
-'e': 'ᥱ',
-'f': '𝖿',
-'g': 'g',
-'h': 'һ',
-'i': 'і',
-'j': 'ȷ',
-'k': 'k',
-'l': 'ᥣ',
-'m': 'm',
-'n': 'ᥒ',
-'o': '᥆',
-'p': '⍴',
-'q': '𝗊',
-'r': 'r',
-'s': 's',
-'t': '𝗍',
-'u': 'ᥙ',
-'v': '᥎',
-'w': 'ᥕ',
-'x': '᥊',
-'y': 'ᥡ',
-'z': 'z'
-}[v.toLowerCase()] || v }))}
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text;
 
-handler.help = ['letra *<texto>*']
-handler.tags = ['fun']
-handler.command = ['letra']
-handler.register = true
+  let transformed = teks.replace(/[a-z]/gi, v => {
+    return {
+      'a': 'ᥲ',
+      'b': 'ᑲ',
+      'c': 'ᥴ',
+      'd': 'ძ',
+      'e': 'ᥱ',
+      'f': '𝖿',
+      'g': 'g',
+      'h': 'һ',
+      'i': 'і',
+      'j': 'ȷ',
+      'k': 'k',
+      'l': 'ᥣ',
+      'm': 'm',
+      'n': 'ᥒ',
+      'o': '᥆',
+      'p': '⍴',
+      'q': '𝗊',
+      'r': 'r',
+      's': 's',
+      't': '𝗍',
+      'u': 'ᥙ',
+      'v': '᥎',
+      'w': 'ᥕ',
+      'x': '᥊',
+      'y': 'ᥡ',
+      'z': 'z'
+    }[v.toLowerCase()] || v;
+  });
 
-export default handler
+  m.reply(`🎃 *Texto transformado por FNaF LATAM:* 🎃\n\n${transformed}`);
+}
+
+handler.help = ['letra *<texto>*'];
+handler.tags = ['fun'];
+handler.command = ['letra'];
+handler.register = true;
+
+export default handler;
