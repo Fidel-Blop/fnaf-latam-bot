aac59a67110e2d93670d3f4caebdbccdb6d1e1ad
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 02:48:14 -0300

    Update rg-deldescription.js

--- plugins/rg-deldescription.js
+++ plugins/rg-deldescription.js
@@ -1,19 +1,23 @@
 import { createHash } from 'crypto';  
 import fetch from 'node-fetch';
 
+const emoji = '✅';
+const emoji2 = '⚠️';
+
 const handler = async (m, { conn }) => {
-let user = global.db.data.users[m.sender];
+  let user = global.db.data.users[m.sender];
 
-if (!user.description) {
-return conn.reply(m.chat, `${emoji2} No tienes una descripción establecida que se pueda eliminar.`, m);
-}
+  if (!user.description) {
+    return conn.reply(m.chat, `${emoji2} No tienes una descripción establecida que se pueda eliminar.`, m);
+  }
 
-user.description = '';
+  user.description = '';
 
-return conn.reply(m.chat, `${emoji} Tu descripción ha sido eliminada.`, m);
+  return conn.reply(m.chat, `${emoji} Tu descripción ha sido eliminada.`, m);
 };
 
-handler.help = ['deldescription']
-handler.tags = ['rg']
-handler.command = ['deldescription', 'deldesc']
+handler.help = ['deldescription'];
+handler.tags = ['rg'];
+handler.command = ['deldescription', 'deldesc'];
+
 export default handler;
