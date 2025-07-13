b0d5074485ccbaf872bafe5396886bc0b465c96b
Fidel-Blop <fidelllgarcia16@gmail.com>
2025-07-13 02:54:48 -0300

    Update rg-setdescription.js

--- plugins/rg-setdescription.js
+++ plugins/rg-setdescription.js
@@ -3,20 +3,20 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
 
-let user = global.db.data.users[m.sender];
+  let user = global.db.data.users[m.sender];
 
-if (user.description) {
-return conn.reply(m.chat, `${emoji2} Ya tienes una descripción establecida, si quieres borrar la descripcion actual usa:\n> » ${usedPrefix}deldescription`, m);
-}
+  if (user.description) {
+    return conn.reply(m.chat, `👹 Ya tienes una descripción establecida.\nSi quieres eliminarla usa:\n> » ${usedPrefix}deldescription`, m);
+  }
 
-if (!text) return conn.reply(m.chat, `${emoji}︎ Debes especificar una descripcion valida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`, m);
+  if (!text) return conn.reply(m.chat, `⚠️ Debes escribir una descripción válida para tu perfil.\n\n> ✐ Ejemplo » *${usedPrefix + command} Fanático de FNaF LATAM*`, m);
 
-user.description = text;
+  user.description = text;
 
-return conn.reply(m.chat, `${emoji} Se ha establecido tu descripcion.\n\n> *${user.description}*`, m);
+  return conn.reply(m.chat, `🦾 Tu descripción ha sido establecida correctamente.\n\n> *${user.description}*`, m);
 };
 
-handler.help = ['setdescription']
-handler.tags = ['rg']
-handler.command = ['setdescription', 'setdesc']
+handler.help = ['setdescription'];
+handler.tags = ['rg'];
+handler.command = ['setdescription', 'setdesc'];
 export default handler;
